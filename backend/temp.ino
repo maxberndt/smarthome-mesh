dfdebacb21b8ef697eab5abf1c06aa5c79c932c4
Max <37291989+maxberndt@users.noreply.github.com>
2019-11-22 18:06:55 +0100

    Temp sensor

---
+++ backend/temp.ino
@@ -0,0 +1,82 @@
+//************************************************************
+// this is a simple example that uses the painlessMesh library
+//
+// 1. sends a silly message to every node on the mesh at a random time between 1 and 5 seconds
+// 2. prints anything it receives to Serial.print
+//
+//
+//************************************************************
+#include "painlessMesh.h"
+#include <math.h>
+
+#define   MESH_PREFIX     "SmartHomeMesh"
+#define   MESH_PASSWORD   "meshit2thelimit"
+#define   MESH_PORT       5555
+
+int sensor = A0;
+
+
+Scheduler userScheduler; // to control your personal task
+painlessMesh  mesh;
+
+// User stub
+void sendMessage() ; // Prototype so PlatformIO doesn't complain
+
+Task taskSendMessage( TASK_SECOND * 5 , TASK_FOREVER, &sendMessage );
+
+
+double getTemp() {
+  double x = analogRead(sensor);
+  double temp = ((0.000011306713 * x * x) - (0.05060512 * x) + 53.995638);
+  return temp;
+}
+
+void sendMessage() {
+  double temp = getTemp();
+  //Serial.printf(String(temp) + " Grad \n");
+  String msg = "T";
+  msg += String(temp);
+  mesh.sendBroadcast(msg);
+  taskSendMessage.setInterval( TASK_SECOND * 10);
+}
+
+// Needed for painless library
+void receivedCallback( uint32_t from, String &msg ) {
+  Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
+}
+
+void newConnectionCallback(uint32_t nodeId) {
+    Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
+}
+
+void changedConnectionCallback() {
+  Serial.printf("Changed connections\n");
+}
+
+void nodeTimeAdjustedCallback(int32_t offset) {
+    Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
+}
+
+void setup() {
+  Serial.begin(115200);
+
+//mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
+  mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
+
+  mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT );
+  mesh.onReceive(&receivedCallback);
+  mesh.onNewConnection(&newConnectionCallback);
+  mesh.onChangedConnections(&changedConnectionCallback);
+  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
+
+  userScheduler.addTask( taskSendMessage );
+  taskSendMessage.enable();
+}
+
+void loop() {
+  // it will run the user scheduler as well
+  mesh.update();
+
+
+  
+}
